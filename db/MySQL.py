804e0b1a0b6f962c2a56f0227c0c27379627df2a
Alexandru Pana <alex.pana@vertexarmy.org>
2013-10-05 13:17:15 +0300

    Added disconnect to the mysql connection api.

--- db/MySQL.py
+++ db/MySQL.py
@@ -5,3 +5,6 @@ class Database():
     def __init__(self, **kwargs):
         self.connection = MySQLdb.connect(**kwargs)
         self.cursor = self.connection.cursor()
+
+    def disconnect(self):
+        self.connection.close()
